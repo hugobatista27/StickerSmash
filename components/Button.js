dabce1cf7b1b69db03b8e0301a995ba5d875a84e
hugobatista27 <hugobatista2710@gmail.com>
2024-07-13 11:18:31 -0300

    feat: cria e adiciona novos buttons

--- components/Button.js
+++ components/Button.js
@@ -22,7 +22,7 @@ export default function Button({ label, theme, onPress }) {
     }
     return (
         <View style={styles.buttonContainer}>
-            <Pressable style={styles.button} onPress={() => alert('clicou')}>
+            <Pressable style={styles.button} onPress={onPress}>
                 <Text style={styles.buttonLabel}>{label}</Text>
             </Pressable>
         </View>
